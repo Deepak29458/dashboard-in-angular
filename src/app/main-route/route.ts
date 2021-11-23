916d2364684e092588a62166bb03893e00887cb9
Deepak <dipun.deepakbehera@gmail.com>
2021-11-23 16:47:24 +0530

    dashboard final

--- src/app/main-route/route.ts
+++ src/app/main-route/route.ts
@@ -1,6 +1,11 @@
 import { Routes } from "@angular/router";
+import { DashboardPanelComponent } from "../dashboard/dashboard-panel/dashboard-panel.component";
 
 export const routes: Routes = [
+    {
+        path:"",
+        component:DashboardPanelComponent,
+    },
     {
         path :"dashboard",
         loadChildren: ()=> import('src/app/dashboard/dashboard.module').then(m => m.DashboardModule),
